74d117958f2967c315686ddf7b1b1afa2b38c369
shantanu400 <ashunayan400@gmail.com>
2024-07-25 17:20:02 +0530

    button error updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Component/Home";
 import Header from "./Component/Header";
 import Viewproduct from "./Component/Viewproduct";
-import { Navigate } from "react-router-dom";
+
 
 function App() {
   

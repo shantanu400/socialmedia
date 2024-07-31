bc4184a7d31716b9644e0a22ee620433a5bf84f4
shantanu400 <ashunayan400@gmail.com>
2024-07-31 21:34:53 +0530

    viewproduct button corrected

--- src/Component/Viewproduct.jsx
+++ src/Component/Viewproduct.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import { useSelector,useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import "bootstrap/dist/css/bootstrap.css";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
@@ -12,18 +12,18 @@ import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 import { useNavigate } from "react-router-dom";
 import VP from "./style/Viewproduct.module.css";
-import { setuserInfo,setchangeId } from "../Slice/userSlice";
+import { setuserInfo, setchangeId } from "../Slice/userSlice";
 import { CiHeart } from "react-icons/ci";
 import { CiShare2 } from "react-icons/ci";
 
 const Viewproduct = () => {
   const user = useSelector((state) => state.user.users);
-  const userInfo=useSelector((state)=>state.user.userInfo);
-  const dispatch=useDispatch();
+  const userInfo = useSelector((state) => state.user.userInfo);
+  const dispatch = useDispatch();
   const { id } = useParams();
   console.log(user.userInfo);
   const person = user.find((eachperson) => eachperson.id === Number(id));
-  const userId=person.userId;
+  const userId = person.userId;
   const navigate = useNavigate();
 
   return (
@@ -51,80 +51,84 @@ const Viewproduct = () => {
           {id}
         </Typography>
       </div>
-
       <div
+        className={VP.insideprodicon}
         style={{
-          
-          marginBottom: "1%",
-          marginLeft: "2%",
-          gap: "2%",
-          display: "flex",
+          backgroundImage: `url(https://picsum.photos/200?random=${person.id})`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
         }}
-        key={person.id}
       >
-       
-          
-          
-          <div className={VP.insideprodicon} style={{backgroundImage:`url(https://picsum.photos/200?random=${person.id})`,backgroundRepeat:"no-repeat",backgroundPosition: 'center',
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        }} >
-            <div className={VP.post}>Post Number</div>
-            <div className={VP.heart}><CiHeart /> </div>
-            <div className={VP.share}><CiShare2 /></div>
-
+        <div className={VP.handle}>
+          <div className={VP.post}>
+            Post Number
+            <div className={VP.logo}>
+              <CiShare2 />
+              <CiHeart />
+            </div>
           </div>
-        
-        <span>
-          <div>
-            <Button
-              variant="contained"
-              style={{
-                backgroundColor: "orange",
-                color: "white",
-                height: "5%",
-                margin: "3%",
-                marginTop: "0%",
-                boxShadow: "2px 2px 20px 0px #F9C5B9",
-              }}
-              onClick={() =>dispatch(setuserInfo(false))}
-            >
-              Details
-            </Button>
-            
-              <Button
-                variant="contained"
-                style={{
-                  backgroundColor: "white",
-                  color: "Black",
-                  height: "5%",
-                  marginTop: "-3%",
-                  boxShadow: "2px 2px 20px 0px #E0E0E0",
-                }}
-                onClick={() =>dispatch(setuserInfo(true))}
+        </div>
+      </div>
+      <div className={VP.buttonpair}>
+        {
+          <div className={VP.detailsubtn}>
+            {user.detailsbtn ? (
+              <div
+                style={{ backgroundColor: "#F05A22", color: "white" }}
+                onClick={() => dispatch(setuserInfo(false))}
+                className={VP.detailsbtn}
               >
-                User Info
-              </Button>
-            
-          </div>
-          <Typography  color="text.secondary">
-            {userInfo && (
-              <div>
-                <p>
-                  <b>UserId:</b> {person.userId}
-                </p>
-                <p>
-                  <b>ID:</b> {person.id}
-                </p>
-                <p>
-                  <b>Title:</b> {person.title}
-                </p>
+                Details
+              </div>
+            ) : (
+              <div
+                style={{ backgroundColor: "#C4C4C4", color: "black" }}
+                onClick={() => dispatch(setuserInfo(false))}
+                className={VP.detailsbtn}
+              >
+                Details
               </div>
             )}
-            {!userInfo && person.body}
-          </Typography>
-        </span>
+
+            {userInfo ? (
+              <div
+                style={{ backgroundColor: "#F05A22", color: "white" }}
+                onClick={() => dispatch(setuserInfo(true))}
+                className={VP.userbtn}
+              >
+                User
+              </div>
+            ) : (
+              <div
+                style={{ backgroundColor: "#C4C4C4", color: "black" }}
+                onClick={() => dispatch(setuserInfo(true))}
+                className={VP.userbtn}
+              >
+                User
+              </div>
+            )}
+          </div>
+        }
+        <div className={VP.detailsuserbody}>
+          {userInfo && (
+            <div>
+              <p>
+                <b>UserId:</b> {person.userId}
+              </p>
+              <p>
+                <b>ID:</b> {person.id}
+              </p>
+              <p>
+                <b>Title:</b> {person.title}
+              </p>
+            </div>
+          )}
+          {!userInfo && person.body}
+        </div>
       </div>
+
+      
+
       <div className={VP.cards}>
         <h2 style={{ marginLeft: "-85%" }}>
           <b>More Posts</b>
